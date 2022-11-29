46b3421917dfbe0aedb69d98e948e223d9ac433d
Emmanuel Hebrard <ehebrard@poulidor.laas.fr>
2022-11-29 13:04:04 +0100

    clique sampler added

---
+++ src/cliquesampler.cpp
@@ -0,0 +1,18 @@
+
+#include "cliquesampler.hpp"
+
+unsigned long gc::xorshf96(void)
+{ // period 2^96-1
+    unsigned long t;
+    x ^= x << 16;
+    x ^= x >> 5;
+    x ^= x << 1;
+
+    t = x;
+    x = y;
+    y = z;
+    z = t ^ x ^ y;
+
+    return z;
+}
+
